703dc7c79c74d0d0961a7149e0a54349b623c22d
Roy Ben Aviv <roybenavivv@gmail.com>
2022-03-03 00:31:05 +0200

    first tests for edit note function

--- js/apps/note/cmps/note-list.cmp.js
+++ js/apps/note/cmps/note-list.cmp.js
@@ -1,11 +1,12 @@
-import noteTxt from '../cmps/note-txt.cmp.js'
-import noteImg from '../cmps/note-img.cmp.js'
-import noteTodos from '../cmps/note-todos.cmp.js'
-import noteVideo from '../cmps/note-video.cmp.js'
+import noteTxt from '../cmps/note-txt.cmp.js';
+import noteImg from '../cmps/note-img.cmp.js';
+import noteTodos from '../cmps/note-todos.cmp.js';
+import noteVideo from '../cmps/note-video.cmp.js';
 
+import noteEdit from '../../note/cmps/note-edit.cmp.js';
 
 export default {
-    
+
     props: ['notes'],
     template: `
         <section class="note-list">
@@ -18,26 +19,42 @@ export default {
 
                            <div class="actions">
                 <button @click="remove(note.id)">X</button>
-                </div>
-                </div>
- 
+                <button @click="edit(note)">Edit</button>
+                            </div>
+                 </div>
+        
             </div>
+            <note-edit :note="currNote" v-if="openEdit"/>
         </section>
     `,
     components: {
         noteTxt,
         noteImg,
         noteTodos,
-        noteVideo
+        noteVideo,
+        noteEdit
+    },
+    data() {
+        return {
+            openEdit: false,
+            currNote: null
+        };
     },
     methods: {
         setNote(text) {
-            console.log(text)
+            console.log(text);
         },
         remove(id) {
             this.$emit('remove', id);
-        }
+        },
+        edit(note) {
+            this.currNote = note
+            console.log(this.currNote);
+            this.openEdit = true;
+            // this.$emit('edit', id)
+        },
+
     },
     computed: {
     }
-}
+};
