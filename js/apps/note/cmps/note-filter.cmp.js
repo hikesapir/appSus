f340844ee1a105ff62a02ac76befd10454e69eb4
Roy Ben Aviv <roybenavivv@gmail.com>
2022-03-03 19:21:48 +0200

    fixed missing tag error

--- js/apps/note/cmps/note-filter.cmp.js
+++ js/apps/note/cmps/note-filter.cmp.js
@@ -5,13 +5,13 @@ export default {
             <input type="radio" id="all" value="" v-model="filterBy.byType" @change="setFilter">
             <label style="font-weight: bold" for="all">All</label> | 
             <input type="radio" id="txt" value="note-txt" v-model="filterBy.byType" @change="setFilter">
-            <label for="txt"><i class="fa-solid fa-t"></i></i></label> |
+            <label for="txt"><i class="fa-solid fa-t"></i></label> |
             <input type="radio" id="img" value="note-img" v-model="filterBy.byType" @change="setFilter">
             <label for="img"><i class="fa-solid fa-image"></i></label> |
             <input type="radio" id="todos" value="note-todos" v-model="filterBy.byType" @change="setFilter">
             <label for="todos"><i class="fa-solid fa-list"></i></label> |
             <input type="radio" id="video" value="note-video" v-model="filterBy.byType" @change="setFilter">
-            <label for="video"><i class="fa-brands fa-youtube"></label> |
+            <label for="video"><i class="fa-brands fa-youtube"></i></label> |
         </section>
     `,
     data() {
