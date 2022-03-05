96e919ab314d5c1c4d992d6693e28c44faeb801f
sapir hiki <sapirhiki@gmail.com>
2022-03-05 19:57:18 +0200

    push

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 import { router } from './router.js';
 import appHeader from './cmps/app-header.cmp.js';
 import appFooter from './cmps/app-footer.cmp.js';
-// import userMsg from './cmps/user-msg.cmp.js';
+import userMsg from './cmps/user-msg.cmp.js';
 
 const options = {
     template: `
         <section>
             <app-header />
-            <!-- <user-msg /> -->
+            <user-msg />
             <router-view/>
             <app-footer />
         </section>
