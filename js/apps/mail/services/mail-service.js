291440fe43c55807181540d0bd653d199d8f85df
Roy Ben Aviv <roybenavivv@gmail.com>
2022-03-05 21:35:33 +0200

    mail subject body changes

--- js/apps/mail/services/mail-service.js
+++ js/apps/mail/services/mail-service.js
@@ -81,7 +81,7 @@ function _setNextPrevMailId(mail) {
     })
 }
 
-function getEmptyMail(subject = '', body = '', sentAt = '', from = '', to = '', isRead = false, isInbox = true, isDraft = false) {
+function getEmptyMail(subject = '', body = '', sentAt = '', from = '', to = '', isRead = false, isInbox = true, isDraft = false, isStarred = false) {
     return {
         id: '',
         subject,
@@ -91,7 +91,7 @@ function getEmptyMail(subject = '', body = '', sentAt = '', from = '', to = '', 
         to,
         isRead,
         isInbox,
-        isStarred: false,
+        isStarred,
         isTrashed: false,
         isDraft,
         isChecked: false
@@ -102,20 +102,20 @@ function _createMails() {
     let mails = utilService.loadFromStorage(MAILS_KEY);
     if (!mails || !mails.length) {
         mails = [];
-        mails.push(_createMail('For Roy ', ' You did really good job!! YOU ARE THE BEST CO thet I could ask for 💪  ', 1646352375050, 'Me', 'me'));
+        mails.push(_createMail('For Roy ', ' You did really good job!! YOU ARE THE BEST CO that I could ask for 💪  ', 1646352375050, 'Me', 'me'));
         mails.push(_createMail('Check your McAfee report now!', 'Your protection at work This is your monthly security report Thank you for letting us keep you safe.', 1646273854871, 'McAfee', 'me'));
         mails.push(_createMail('Sign in to CSSBattle', 'We received a request to sign in to CSSBattle using this email address. If you want to sign in with your user@appsus.com account, click this link: Sign in to CSSBattle If you did not request this link, you can safely ignore this email. Thanks, Your CSSBattle team', 1646215582260, 'CSSBattle', 'me'));
         mails.push(_createMail('New messages from Matan Crispel', ' hey, We deleted your folders in Dropbox by Thursday. please don\'t forget DO NOT COPY YOUR GIT FOLDER TO THE DROPBOX', 1646215512260, 'DROPBOX', 'me'));
         mails.push(_createMail('CSSBattle', 'please join me to CSSBattle 🙏🙏🙏 \n Roy ', 1646215512260, 'Roy', 'me'));
         mails.push(_createMail('Your acceptance on order google API', 'Order Number:6424-7519-71   Subtotal:130$ for useing google maps API ', 1646215512260, 'GoogleAPI', 'me'));
-        mails.push(_createMail('test', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet rem nulla sit consequatur odit nobis vel libero! Repellendus, quidem alias est officia veritatis, ex laudantium eius, facere excepturi impedit quaerat.', 1616211177545, 'Test', 'me'));
+        mails.push(_createMail('For Rotem', 'Thank you for giving us motivation and support during the sprint! much love!', 1616211177545, 'Roy & Sapir', 'Rotem', false, true, false, true));
         utilService.saveToStorage(MAILS_KEY, mails);
     }
     return mails;
 }
 
-function _createMail(subject, body, sentAt, from, to, isRead, isInbox, isDraft) {
-    const mail = getEmptyMail(subject, body, sentAt, from, to, isRead, isInbox, isDraft)
+function _createMail(subject, body, sentAt, from, to, isRead, isInbox, isDraft, isStarred) {
+    const mail = getEmptyMail(subject, body, sentAt, from, to, isRead, isInbox, isDraft, isStarred)
     mail.id = utilService.makeId()
     return mail;
 }
